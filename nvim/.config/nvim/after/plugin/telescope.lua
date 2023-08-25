63747c731b02b7bf1fde9069b6a8feef2c08494f
jeevanpranav02 <jellybean02092002@gmail.com>
2023-08-26 01:37:34 +0530

    fix: made trouble config

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -11,18 +11,3 @@ require('telescope').load_extension('media_files')
 
 vim.keymap.set('n', '<leader>me', '<cmd>Telescope media_files<CR>')
 
-local trouble = require("trouble.providers.telescope")
-
-
-require('telescope').setup {
-    defaults = {
-        file_ignore_patterns = {
-            "node_modules",
-            "target"
-        },
-        mappings = {
-            i = { ["<c-t>"] = trouble.open_with_trouble },
-            n = { ["<c-t>"] = trouble.open_with_trouble },
-        },
-    }
-}
