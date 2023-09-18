e3fb48cee75760a3c57a020d59fa4751cd42262a
Jeevan Pranav <jellybean02092002@gmail.com>
2023-09-18 16:01:52 +0530

    tweeks

--- nvim/.config/nvim/lua/JellyBean/packer.lua
+++ nvim/.config/nvim/lua/JellyBean/packer.lua
@@ -47,7 +47,8 @@ return require('packer').startup(function(use)
         'akinsho/flutter-tools.nvim',
         requires = {
             'nvim-lua/plenary.nvim',
-            'stevearc/dressing.nvim', -- optional for vim.ui.select
+            'stevearc/dressing.nvim',             -- optional for vim.ui.select
+
         },
     }
 
@@ -75,21 +76,21 @@ return require('packer').startup(function(use)
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
         requires = { -- LSP Support
-        { 'neovim/nvim-lspconfig' },
-        {
-            'williamboman/mason.nvim',
-            run = ":MasonUpdate"
-        },
-        { 'williamboman/mason-lspconfig.nvim' },
-        -- Autocompletion
-        { 'hrsh7th/nvim-cmp' },
-        { 'hrsh7th/cmp-buffer' },
-        { 'hrsh7th/cmp-path' },
-        { 'saadparwaiz1/cmp_luasnip' },
-        { 'hrsh7th/cmp-nvim-lsp' },
-        { 'hrsh7th/cmp-nvim-lua' },
-        -- Snippets
-        { 'L3MON4D3/LuaSnip' },
-        { 'rafamadriz/friendly-snippets' } }
+            { 'neovim/nvim-lspconfig' },
+            {
+                'williamboman/mason.nvim',
+                run = ":MasonUpdate"
+            },
+            { 'williamboman/mason-lspconfig.nvim' },
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' } }
     }
 end)
