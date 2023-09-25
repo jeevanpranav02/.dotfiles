ee45e06333ab3d7d2cb93c49d011ade4e44a02f1
Jeevan Pranav <jellybean02092002@gmail.com>
2023-09-26 03:01:01 +0530

    added neorg config

---
+++ nvim/.config/nvim/after/plugin/neorg.lua
@@ -0,0 +1,55 @@
+require('neorg').setup {
+    load = {
+        ["core.defaults"] = {},
+        ["core.completion"] = {
+            config = {
+                engine = "nvim-cmp",
+            },
+        },
+        ["core.qol.toc"] = {
+            config = {
+                close_after_use = true,
+            },
+        },
+        ["core.integrations.treesitter"] = {},
+        ["core.concealer"] = {},
+        ["core.mode"] = {},
+        ["core.promo"] = {},
+        ["core.ui"] = {},
+        ["core.itero"] = {},
+        ["core.ui.calendar"] = {},
+        ["core.queries.native"] = {},
+        ["core.summary"] = {},
+        ["core.presenter"] = {
+            config = {
+                zen_mode = "zen-mode",
+            }
+        },
+        ["core.dirman"] = {
+            config = {
+                workspaces = {
+                    work     = "~/development/notes/work",
+                    tn2s     = "~/development/notes/TN2S",
+                    personal = "~/development/notes/personal",
+                },
+                default_workspace = "personal",
+            },
+        },
+        ["core.keybinds"] = {
+            config = {
+                default_keybinds = true,
+                neorg_leader = " ",
+            },
+        },
+        ["core.export"] = {
+            config = {
+                export_dir = "~/notes/"
+            },
+        },
+        ["core.export.markdown"] = {
+            config = {
+                extensions = "all",
+            },
+        },
+    }
+}
