96441767236864403a1d1222f69fbf5a4ec19b19
Jeevan Pranav <jellybean02092002@gmail.com>
2023-09-26 01:00:08 +0530

    change in keymaps

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,11 +1,11 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
-vim.keymap.set('n', '<leader>gr', builtin.lsp_references,{})
-vim.keymap.set('n', '<leader>gi', builtin.lsp_implementations,{})
+vim.keymap.set('n', '<leader>gr', builtin.lsp_references, {})
+vim.keymap.set('n', '<leader>gi', builtin.lsp_implementations, {})
 
 require('telescope').load_extension('media_files')
 
@@ -23,5 +23,25 @@ require('telescope').setup {
             "ios",
             "macos"
         },
+        initial_mode = "normal",
+    },
+    pickers = {
+        find_files = {
+            theme = "dropdown",
+            previewer = false,
+        },
+        grep_string = {
+            theme = "dropdown",
+            previewer = false,
+        },
+        git_files = {
+            theme = "dropdown",
+            previewer = false,
+        },
+    },
+    extensions = {
+        "media_files",
+        "fzf",
+        "file_browser",
     }
 }
