3539661c684e57d1e15d47a9c162feaeec5eea6c
Jeevan Pranav <jellybean02092002@gmail.com>
2023-09-29 18:18:43 +0530

    fix(colors): colorizer for flutter colors

--- nvim/.config/nvim/after/plugin/colorizer.lua
+++ nvim/.config/nvim/after/plugin/colorizer.lua
@@ -4,9 +4,9 @@ require("colorizer").setup {
         RGB = true,          -- #RGB hex codes
         RRGGBB = true,       -- #RRGGBB hex codes
         names = true,        -- "Name" codes like Blue or blue
-        RRGGBBAA = false,    -- #RRGGBBAA hex codes
-        AARRGGBB = false,    -- 0xAARRGGBB hex codes
-        rgb_fn = false,      -- CSS rgb() and rgba() functions
+        RRGGBBAA = true,    -- #RRGGBBAA hex codes
+        AARRGGBB = true,    -- 0xAARRGGBB hex codes
+        rgb_fn = true,      -- CSS rgb() and rgba() functions
         hsl_fn = false,      -- CSS hsl() and hsla() functions
         css = true,          -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
         css_fn = true,       -- Enable all CSS *functions*: rgb_fn, hsl_fn
