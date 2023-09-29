369c2ac623b0f3a4b7f33e0b8b1fb08457bb579c
Jeevan Pranav <jellybean02092002@gmail.com>
2023-09-29 18:16:28 +0530

    feat(zen): added zen-mode

---
+++ nvim/.config/nvim/after/plugin/zen-mode.lua
@@ -0,0 +1,23 @@
+if not pcall(require, "zen-mode") then
+  return
+end
+
+require("zen-mode").setup {
+  window = {
+    backdrop = 1,
+    height = 0.9,
+    -- width = 140,
+    options = {
+      number = false,
+      relativenumber = false,
+      signcolumn = "no",
+      list = false,
+      cursorline = false,
+    },
+  },
+}
+
+require("twilight").setup {
+  context = -1,
+  treesitter = true,
+}
