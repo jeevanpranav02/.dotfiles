1acf71195ea4230c81f0d66988db4dd47b9848b4
Jeevan Pranav <jellybean02092002@gmail.com>
2023-09-29 18:20:10 +0530

    lsp tweeks

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -63,6 +63,40 @@ cmp.setup({
             },
         },
     },
+    sorting = {
+        -- TODO: Would be cool to add stuff like "See variable names before method names" in rust, or something like that.
+        comparators = {
+            cmp.config.compare.offset,
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+
+            -- copied from cmp-under, but I don't think I need the plugin for this.
+            -- I might add some more of my own.
+            function(entry1, entry2)
+                local _, entry1_under = entry1.completion_item.label:find "^_+"
+                local _, entry2_under = entry2.completion_item.label:find "^_+"
+                entry1_under = entry1_under or 0
+                entry2_under = entry2_under or 0
+                if entry1_under > entry2_under then
+                    return false
+                elseif entry1_under < entry2_under then
+                    return true
+                end
+            end,
+
+            cmp.config.compare.kind,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+        },
+    },
+    experimental = {
+        -- I like the new menu better! Nice work hrsh7th
+        native_menu = false,
+
+        -- Let's play with this for a day or two
+        ghost_text = true,
+    },
 })
 
 lsp.set_preferences({
@@ -106,6 +140,9 @@ lspconfig.rust_analyzer.setup {}
 
 require("flutter-tools").setup {
     config = {
+        widget_guides = {
+            enabled = true,
+        },
         lsp = {
             capabilities = capabilities,
         },
