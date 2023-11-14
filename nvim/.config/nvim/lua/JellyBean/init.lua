f860d04589f01ae9ccb41a73e37d184340908c5e
Jeevan Pranav <jellybean02092002@gmail.com>
2023-11-15 01:38:19 +0530

    feat: separate files for autocmds

--- nvim/.config/nvim/lua/JellyBean/init.lua
+++ nvim/.config/nvim/lua/JellyBean/init.lua
@@ -1,36 +1,4 @@
 require("JellyBean.set")
 require("JellyBean.remap")
 require("JellyBean.lazy")
-
-local augroup = vim.api.nvim_create_augroup
-local JellyBeanGroup = augroup('JellyBean', {})
-
-local autocmd = vim.api.nvim_create_autocmd
-local yank_group = augroup('HighlightYank', {})
-
-function R(name)
-    require("plenary.reload").reload_module(name)
-end
-
-autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
-})
-
-autocmd({"BufWritePre"}, {
-    group = JellyBeanGroup,
-    pattern = "*",
-    command = [[%s/\s\+$//e]],
-})
-
-vim.cmd[[
-    augroup file_blade_php
-      autocmd BufRead,BufNewFile *.blade.php setlocal ts=2 sts=2 sw=2 filetype=blade syntax=blade expandtab
-    augroup END
-]]
+require("JellyBean.autocmd")
