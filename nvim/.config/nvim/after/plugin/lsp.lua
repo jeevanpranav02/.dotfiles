1d42fbafbbfd10b7d9ab9de59324e78b69131b6f
Jeevan Pranav <jellybean02092002@gmail.com>
2023-11-15 01:35:04 +0530

    feat: laravel and php support

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -159,20 +159,81 @@ lspconfig.lua_ls.setup {
     },
 }
 
+-- Php and Laravel Setup
+lspconfig.intelephense.setup({
+    commands = {
+        IntelephenseIndex = {
+            function()
+                vim.lsp.buf.execute_command({ command = 'intelephense.index.workspace' })
+            end,
+        },
+    },
+    on_attach = on_attach,
+    capabilities = capabilities
+})
+
+lspconfig.phpactor.setup({
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+        client.server_capabilities.completionProvider = false
+        client.server_capabilities.hoverProvider = false
+        client.server_capabilities.implementationProvider = false
+        client.server_capabilities.referencesProvider = false
+        client.server_capabilities.renameProvider = false
+        client.server_capabilities.selectionRangeProvider = false
+        client.server_capabilities.signatureHelpProvider = false
+        client.server_capabilities.typeDefinitionProvider = false
+        client.server_capabilities.workspaceSymbolProvider = false
+        client.server_capabilities.definitionProvider = false
+        client.server_capabilities.documentHighlightProvider = false
+        client.server_capabilities.documentSymbolProvider = false
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+    end,
+    init_options = {
+        ["language_server_phpstan.enabled"] = false,
+        ["language_server_psalm.enabled"] = false,
+    },
+    handlers = {
+        ['textDocument/publishDiagnostics'] = function() end
+    },
+})
+
+-- Vue, JavaScript, TypeScript
+lspconfig.volar.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    -- Enable "Take Over Mode" where volar will provide all JS/TS LSP services
+    -- This drastically improves the responsiveness of diagnostic updates on change
+    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+})
+
+-- Tailwind CSS
+lspconfig.tailwindcss.setup({ capabilities = capabilities })
+
+-- JSON
+lspconfig.jsonls.setup({
+    capabilities = capabilities,
+    settings = {
+        json = {
+            schemas = require('schemastore').json.schemas(),
+        },
+    },
+})
 
 -- Flutter Tools Setup
 require("flutter-tools").setup {
     debugger = {
         enabled = true,
-        run_via_dap = true,
-        exception_breakpoints = { "raised", "uncaught" },
+        run_via_dap = false,
+        exception_breakpoints = ({ "raised", "uncaught" }),
     },
     outline = { auto_open = false },
     decorations = {
         statusline = { device = true, app_version = true },
     },
     widget_guides = { enabled = true, debug = true },
-    dev_log = { enabled = true, open_cmd = "tabedit" },
+    dev_log = { enabled = true, notify_errors = true, open_cmd = "tabedit", },
     lsp = {
         color = {
             enabled = true,
--- nvim/.config/nvim/after/queries/blade/injections.scm
+++ nvim/.config/nvim/after/queries/blade/injections.scm
@@ -5,4 +5,6 @@
     (#set! injection.language "php")
     (#set! injection.combined))
 ((parameter) @injection.content
-    (#set! injection.language php))
+    (#set! injection.language "php")) ; TODO: php_only
+((php_only) @injection.content
+    (#set! injection.language "php")) ; TODO: php_only
