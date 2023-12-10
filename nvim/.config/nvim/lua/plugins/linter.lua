34cbfb9622ada20260a8442c6c7d86d52bbade49
Jeevan Pranav <jellybean02092002@gmail.com>
2023-12-10 12:24:53 +0530

    added linter

--- nvim/.config/nvim/lua/plugins/linter.lua
+++ nvim/.config/nvim/lua/plugins/linter.lua
@@ -1,20 +1,6 @@
 require("lint").linters_by_ft = {
-	javascript = { "volar" },
-	typescript = { "volar" },
-	typescriptreact = { "volar" },
-	vue = { "volar" },
-	html = { "vale" },
-	css = { "volar" },
-	scss = { "volar" },
-	sass = { "stylelint" },
-	markdown = { "vale" },
-	json = { "jsonlint" },
-	yaml = { "yamllint" },
 	php = { "phpcs" },
 	blade = { "phpcs" },
-	twig = { "phpcs" },
-	rust = { "cargo" },
-	go = { "golangcilint" },
 }
 
 local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
