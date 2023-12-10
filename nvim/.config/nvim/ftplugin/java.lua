6ea362b930bb3195aeeffd8beb17cfa2c9dd5e61
Jeevan Pranav <jellybean02092002@gmail.com>
2023-12-10 12:16:19 +0530

    javastuff

--- nvim/.config/nvim/ftplugin/java.lua
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -2,33 +2,63 @@ local status, jdtls = pcall(require, "jdtls")
 if not status then
 	return
 end
-
+local api = vim.api
 local home = os.getenv("HOME")
 
 local root_markers = { "gradlew", "mvnw", ".git" }
 local root_dir = require("jdtls.setup").find_root(root_markers)
 
-local workspace_folder = home .. "/.cache/jdtls/workspace" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+jdtls.jol_path = os.getenv("HOME") .. "/tools/jol.jar"
 
-local bundles = {
-	vim.fn.glob(
-		"home/jp/tools/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar"
-	),
-	vim.fn.glob("/home/jp/tools/vscode-java-test/*.jar"),
+local workspace_folder = home .. "/.cache/jdtls/workspace" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+local jar_patterns = {
+	"/tools/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar",
+	"/tools/vscode-java-decompiler/server/*.jar",
+	"/tools/vscode-java-test/java-extension/com.microsoft.java.test.plugin/target/*.jar",
+	"/tools/vscode-java-test/java-extension/com.microsoft.java.test.runner/target/*.jar",
+	"/tools/vscode-java-test/java-extension/com.microsoft.java.test.runner/lib/*.jar",
 }
+local plugin_path =
+	"/tools/vscode-java-test/java-extension/com.microsoft.java.test.plugin.site/target/repository/plugins/"
+
+local bundle_list = vim.tbl_map(function(x)
+	return require("jdtls.path").join(plugin_path, x)
+end, {
+	"junit-jupiter-*.jar",
+	"junit-platform-*.jar",
+	"junit-vintage-engine_*.jar",
+	"org.opentest4j*.jar",
+	"org.apiguardian.api_*.jar",
+	"org.eclipse.jdt.junit4.runtime_*.jar",
+	"org.eclipse.jdt.junit5.runtime_*.jar",
+	"org.opentest4j_*.jar",
+})
+vim.list_extend(jar_patterns, bundle_list)
+local bundles = {}
+for _, jar_pattern in ipairs(jar_patterns) do
+	for _, bundle in ipairs(vim.split(vim.fn.glob(home .. jar_pattern), "\n")) do
+		if
+			not vim.endswith(bundle, "com.microsoft.java.test.runner-jar-with-dependencies.jar")
+			and not vim.endswith(bundle, "com.microsoft.java.test.runner.jar")
+		then
+			table.insert(bundles, bundle)
+		end
+	end
+end
 
 local extendedClientCapabilities = jdtls.extendedClientCapabilities
-extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
+-- extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
+extendedClientCapabilities.onCompletionItemSelectedCommand = "editor.action.triggerParameterHints"
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local on_attach = function(client, bufnr)
-	require("jdtls").setup_dap({ hotcodereplace = "auto" })
 	local status_ok, jdtls_dap = pcall(require, "jdtls.dap")
 	if status_ok then
 		jdtls_dap.setup_dap_main_class_configs()
 	end
+
 	local _, _ = pcall(vim.lsp.codelens.refresh)
 	vim.lsp.codelens.refresh()
 
@@ -36,6 +66,14 @@ local on_attach = function(client, bufnr)
 		silent = true,
 		buffer = bufnr,
 	}
+
+	vim.keymap.set("n", "<leader>0", function()
+		if vim.lsp.inlay_hint.is_enabled(bufnr) then
+			vim.lsp.inlay_hint.enable(bufnr, false)
+		else
+			vim.lsp.inlay_hint.enable(bufnr, true)
+		end
+	end, opts)
 
 	vim.keymap.set("n", "<C-o>", jdtls.organize_imports, {
 		desc = "Organize imports",
@@ -60,6 +98,19 @@ local on_attach = function(client, bufnr)
 		desc = "Extract constant",
 		buffer = bufnr,
 	})
+
+	vim.keymap.set("n", "<leader>ds", function()
+		local dap = require("dap")
+		if dap.session() then
+			local widgets = require("dap.ui.widgets")
+			widgets.centered_float(widgets.scopes)
+		else
+			client.request_sync("java/buildWorkspace", false, 5000, bufnr)
+			require("jdtls.dap").pick_test()
+		end
+	end, opts)
+
+	require("jdtls").setup_dap({ hotcodereplace = "auto" })
 end
 local config = {
 	capabilities = capabilities,
@@ -74,17 +125,12 @@ local config = {
 	},
 	settings = {
 		java = {
+			maxConcurrentBuilds = 8,
 			eclipse = {
 				downloadSources = true,
 			},
 			saveActions = {
 				organizeImports = true,
-			},
-			format = {
-				settings = {
-					url = home .. ".config/nvim/lang-servers/intellij-java-google-style.xml",
-					profile = "GoogleStyle",
-				},
 			},
 			maven = {
 				downloadSources = true,
