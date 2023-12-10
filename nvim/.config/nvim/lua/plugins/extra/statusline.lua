1c2fe34b6bc6680580fea3fa2bf5fa9e17de5000
Jeevan Pranav <jellybean02092002@gmail.com>
2023-12-10 12:23:34 +0530

    just statusline

--- nvim/.config/nvim/lua/plugins/extra/statusline.lua
+++ nvim/.config/nvim/lua/plugins/extra/statusline.lua
@@ -1,59 +1,174 @@
-local sl_hl = vim.api.nvim_get_hl_by_name("StatusLine", true)
-vim.api.nvim_set_hl(0, "Copilot", { fg = "#6CC644", bg = sl_hl.background })
-local icons = require("ui.icons")
-local diff = {
-	"diff",
-	colored = true,
-	symbols = { added = icons.git.LineAdded, modified = icons.git.LineModified, removed = icons.git.LineRemoved }, -- Changes the symbols used by the diff.
-}
+require("el").reset_windows()
 
-local copilot = function()
-	local buf_clients = vim.lsp.get_active_clients({ bufnr = 0 })
-	if #buf_clients == 0 then
-		return "LSP Inactive"
-	end
+vim.opt.laststatus = 3
 
-	local buf_client_names = {}
-	local copilot_active = false
+if false then
+	-- Disappearing statusline for commands
+	vim.opt.cmdheight = 0
+	vim.api.nvim_create_autocmd("ModeChanged", {
+		group = vim.api.nvim_create_augroup("StatusDisappear", { clear = true }),
+		callback = function()
+			if vim.v.event.new_mode == "c" then
+				vim.opt.laststatus = 0
+			elseif vim.v.event.old_mode == "c" then
+				vim.opt.laststatus = 3
+			end
 
-	for _, client in pairs(buf_clients) do
-		if client.name ~= "null-ls" and client.name ~= "copilot" then
-			table.insert(buf_client_names, client.name)
-		end
-
-		if client.name == "copilot" then
-			copilot_active = true
-		end
-	end
-
-	if copilot_active then
-		return "%#Copilot#" .. icons.git.Octoface .. "%*"
-	end
-	return ""
+			pcall(vim.cmd, [[silent! redraw]])
+		end,
+	})
 end
 
-require("lualine").setup({
-	options = {
-		-- component_separators = { left = "", right = "" },
-		-- section_separators = { left = "", right = "" },
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
+local builtin = require("el.builtin")
+local extensions = require("el.extensions")
+local sections = require("el.sections")
+local subscribe = require("el.subscribe")
+local lsp_statusline = require("el.plugins.lsp_status")
+local diagnostic = require("el.diagnostic")
 
-		ignore_focus = { "NvimTree" },
+local has_lsp_extensions, ws_diagnostics = pcall(require, "lsp_extensions.workspace.diagnostic")
+
+sections.filetype("alpha", "")
+
+-- TODO: Spinning planet extension. Integrated w/ telescope.
+-- ◐ ◓ ◑ ◒
+-- 🌛︎🌝︎🌜︎🌚︎
+-- Show telescope icon / emoji when you open it as well
+
+local git_icon = subscribe.buf_autocmd("el_file_icon", "BufRead", function(_, bufnr)
+	local icon = extensions.file_icon(_, bufnr)
+	if icon then
+		return icon .. " "
+	end
+
+	return ""
+end)
+
+local git_branch = subscribe.buf_autocmd("el_git_branch", "BufEnter", function(window, buffer)
+	local branch = extensions.git_branch(window, buffer)
+	if branch then
+		return " " .. extensions.git_icon() .. " " .. branch
+	end
+end)
+
+local git_changes = subscribe.buf_autocmd("el_git_changes", "BufWritePost", function(window, buffer)
+	return extensions.git_changes(window, buffer)
+end)
+
+local ws_diagnostic_counts = function(_, buffer)
+	if not has_lsp_extensions then
+		return ""
+	end
+
+	local messages = {}
+
+	local error_count = ws_diagnostics.get_count(buffer.bufnr, "Error")
+
+	local x = "⬤"
+	if error_count == 0 then
+	-- pass
+	elseif error_count < 5 then
+		table.insert(messages, string.format("%s#%s#%s%%*", "%", "StatuslineError" .. error_count, x))
+	else
+		table.insert(messages, string.format("%s#%s#%s%%*", "%", "StatuslineError5", x))
+	end
+
+	return table.concat(messages, "")
+end
+
+local show_current_func = function(window, buffer)
+	if buffer.filetype == "lua" then
+		return ""
+	end
+
+	return lsp_statusline.current_function(window, buffer)
+end
+
+local minimal_status_line = function(_, buffer)
+	if string.find(buffer.name, "sourcegraph/sourcegraph") then
+		return true
+	end
+end
+
+local is_sourcegraph = function(_, buffer)
+	if string.find(buffer.name, "sg://") then
+		return true
+	end
+end
+
+local diagnostic_display = diagnostic.make_buffer()
+
+require("el").setup({
+	generator = function(window, buffer)
+		local is_minimal = minimal_status_line(window, buffer)
+		local is_sourcegraph = is_sourcegraph(window, buffer)
+
+		local mode = extensions.gen_mode({ format_string = " %s " })
+		if is_sourcegraph then
+			return {
+				{ mode },
+				{ sections.split, required = true },
+				{ builtin.file },
+				{ sections.split, required = true },
+				{ builtin.filetype },
+			}
+		end
+
+		local items = {
+			{ mode, required = true },
+			{ git_branch },
+			{ " " },
+			{ sections.split, required = true },
+			{ git_icon },
+			{ sections.maximum_width(builtin.file_relative, 0.60), required = true },
+			{ sections.collapse_builtin({ { " " }, { builtin.modified_flag } }) },
+			{ sections.split, required = true },
+			{ diagnostic_display },
+			{ show_current_func },
+			-- { lsp_statusline.server_progress },
+			-- { ws_diagnostic_counts },
+			{ git_changes },
+			{ "[" },
+			{ builtin.line_with_width(3) },
+			{ ":" },
+			{ builtin.column_with_width(2) },
+			{ "]" },
+			{
+				sections.collapse_builtin({
+					"[",
+					builtin.help_list,
+					builtin.readonly_list,
+					"]",
+				}),
+			},
+			{ builtin.filetype },
+		}
+
+		local add_item = function(result, item)
+			if is_minimal and not item.required then
+				return
+			end
+
+			table.insert(result, item)
+		end
+
+		local result = {}
+		for _, item in ipairs(items) do
+			add_item(result, item)
+		end
+
+		return result
+	end,
+})
+
+require("fidget").setup({
+	text = {
+		spinner = "moon",
 	},
-	sections = {
-		-- lualine_a = { {"branch", icon =""} },
-		-- lualine_b = { diff },
-		-- lualine_c = { "diagnostics" },
-		-- lualine_x = { copilot },
-		-- lualine_y = { "filetype" },
-		-- lualine_z = { "progress" },
-		lualine_a = { "mode" },
-		lualine_b = { "branch" },
-		lualine_c = { diff },
-		lualine_x = { "diagnostics", copilot },
-		lualine_y = { "filetype" },
-		lualine_z = { "progress" },
+	align = {
+		bottom = true,
 	},
-	extensions = { "quickfix", "man", "fugitive" },
+	window = {
+		relative = "editor",
+	},
 })
