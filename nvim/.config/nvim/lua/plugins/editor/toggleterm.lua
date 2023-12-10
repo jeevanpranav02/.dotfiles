9c87636cfd9bbe5d8cdd6cda2cb12c82f1b31c5c
Jeevan Pranav <jellybean02092002@gmail.com>
2023-12-10 12:22:17 +0530

    added terminal support

--- nvim/.config/nvim/lua/plugins/editor/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/editor/toggleterm.lua
@@ -9,11 +9,10 @@ require("toggleterm").setup({
 	open_mapping = [[<c-\>]],
 	shade_filetypes = {},
 	shade_terminals = true,
-	shading_factor = 1,
-	start_in_insert = true,
+	shading_factor = 2,
 	insert_mappings = true,
 	persist_size = true,
-	direction = "horizontal",
+	direction = "float",
 	close_on_exit = true,
 	shell = vim.o.shell,
 	float_opts = {
