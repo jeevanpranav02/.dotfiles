0aed65662b23aaf0b35fcb8e17a8730b0ad2d59c
Jeevan Pranav <jellybean02092002@gmail.com>
2023-12-22 01:22:21 +0530

    fix: improve blade syntax highlighting

--- nvim/.config/nvim/queries/blade/highlights.scm
+++ nvim/.config/nvim/queries/blade/highlights.scm
@@ -1,27 +1,27 @@
-; (directive) @function
-; (directive_start) @function
-; (directive_end) @function
-; (comment) @comment
-; ((parameter) @include (#set! "priority" 110))
-; ((php_only) @include (#set! "priority" 110))
-; ((bracket_start) @function (#set! "priority" 120))
-; ((bracket_end) @function (#set! "priority" 120))
-; (keyword) @function
-;
+(directive) @function
+(directive_start) @function
+(directive_end) @function
+(comment) @comment
+((parameter) @include (#set! "priority" 110))
+((php_only) @include (#set! "priority" 110))
+((bracket_start) @function (#set! "priority" 120))
+((bracket_end) @function (#set! "priority" 120))
+(keyword) @function
+
 ; (directive) @tag
 ; (directive_start) @tag
 ; (directive_end) @tag
 ; ; (bracket) @tag.delimiter
 ; (comment) @comment @spell
-[
-  (directive)
-  (directive_start)
-  (directive_end)
-] @keyword
-
-([
-  (bracket_start)
-  (bracket_end)
-] @punctuation.special (#set! "priority" 110))
-
-(comment) @comment @spell
+; [
+;   (directive)
+;   (directive_start)
+;   (directive_end)
+; ] @keyword
+;
+; ([
+;   (bracket_start)
+;   (bracket_end)
+; ] @punctuation.special (#set! "priority" 110))
+;
+; (comment) @comment @spell
