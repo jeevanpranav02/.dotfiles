4db281de92d1d7fee1fdbf8ca44ba0f6407fa8da
Jeevan Pranav <jellybean02092002@gmail.com>
2023-12-28 01:17:40 +0530

    feat: ai thingy

---
+++ nvim/.config/nvim/lua/plugins/extra/sg-cody.lua
@@ -0,0 +1,34 @@
+return {
+	"sourcegraph/sg.nvim",
+	enabled = false,
+	dependencies = {
+		"nvim-lua/plenary.nvim", --[[ "nvim-telescope/telescope.nvim ]]
+	},
+	config = function() -- Toggle cody chat
+		vim.keymap.set("n", "<space>cc", function()
+			require("sg.cody.commands").toggle()
+		end)
+
+		vim.keymap.set("n", "<space>cn", function()
+			local name = vim.fn.input("chat name: ")
+			require("sg.cody.commands").chat(name)
+		end)
+		-- vim.keymap.set("v", "<space>ca", ":CodyContext<CR>")
+		-- vim.keymap.set("v", "<space>ce", ":CodyExplain<CR>")
+
+		vim.keymap.set("n", "<space>ss", function()
+			require("sg.extensions.telescope").fuzzy_search_results()
+		end)
+
+		local ok, msg = pcall(require, "sg")
+		if not ok then
+			print("sg failed to load with:", msg)
+			return
+		end
+
+		require("sg").setup({
+			on_attach = require("jb.lsputils").on_attach,
+			enable_cody = true,
+		})
+	end,
+}
