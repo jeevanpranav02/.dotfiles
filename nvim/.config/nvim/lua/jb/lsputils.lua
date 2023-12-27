8585f5e0d0994b98a60e8ee83fc4528d44b00be6
Jeevan Pranav <jellybean02092002@gmail.com>
2023-12-28 01:17:08 +0530

    refactor: added common attach and capabilities

---
+++ nvim/.config/nvim/lua/jb/lsputils.lua
@@ -0,0 +1,50 @@
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+local on_attach = function(client, bufnr)
+	local opts = { buffer = bufnr, remap = false }
+	-- print(client.name .. ": Hello there.")
+	vim.keymap.set("n", "<leader>0", function()
+		if vim.lsp.inlay_hint.is_enabled(bufnr) then
+			vim.lsp.inlay_hint.enable(bufnr, false)
+		else
+			vim.lsp.inlay_hint.enable(bufnr, true)
+		end
+	end, opts)
+
+	vim.keymap.set("n", "<leader>gd", function()
+		vim.lsp.buf.definition()
+	end, opts)
+	vim.keymap.set("n", "K", function()
+		vim.lsp.buf.hover()
+	end, opts)
+	vim.keymap.set("n", "<leader>vws", function()
+		vim.lsp.buf.workspace_symbol()
+	end, opts)
+	vim.keymap.set("n", "<leader>vd", function()
+		vim.diagnostic.open_float()
+	end, opts)
+	vim.keymap.set("n", "[d", function()
+		vim.diagnostic.goto_next()
+	end, opts)
+	vim.keymap.set("n", "]d", function()
+		vim.diagnostic.goto_prev()
+	end, opts)
+	vim.keymap.set("n", "<leader>vca", function()
+		vim.lsp.buf.code_action()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrr", function()
+		vim.lsp.buf.references()
+	end, opts)
+	vim.keymap.set("n", "<leader>vrn", function()
+		vim.lsp.buf.rename()
+	end, opts)
+	vim.keymap.set("i", "<C-h>", function()
+		vim.lsp.buf.signature_help()
+	end, opts)
+end
+
+local M = {}
+
+M.on_attach = on_attach
+M.capabilities = capabilities
+return M
