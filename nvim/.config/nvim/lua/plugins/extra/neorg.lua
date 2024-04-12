eba49560ee7646cea0e0d83fb0a119bbcb98e54e
Jeevan Pranav <jellybean02092002@gmail.com>
2024-04-12 13:19:15 +0530

    fix(nvim): neorg migration

--- nvim/.config/nvim/lua/plugins/extra/neorg.lua
+++ nvim/.config/nvim/lua/plugins/extra/neorg.lua
@@ -1,65 +1,80 @@
 return {
-	"nvim-neorg/neorg",
-	lazy = true,
-	build = ":Neorg sync-parsers",
-	ft = "norg",
-	cmd = "Neorg",
-	priority = 30,
-	dependencies = { { "nvim-lua/plenary.nvim" }, { "godlygeek/tabular" } },
-	config = function()
-		require("neorg").setup({
-			load = {
-				["core.defaults"] = {},
-				-- ["core.completion"] = {
-				-- 	config = {
-				-- 		engine = "nvim-cmp",
-				-- 	},
-				-- },
-				["core.clipboard.code-blocks"] = {},
-				["core.autocommands"] = {},
-				["core.esupports.indent"] = {},
-				["core.qol.toc"] = {
-					config = {
-						close_split_on_jump = false,
-					},
-				},
-				["core.integrations.treesitter"] = {},
-				["core.export"] = {},
-				["core.export.markdown"] = {
-					config = {
-						extension = "md",
-						extenstions = "all",
-					},
-				},
-				["core.concealer"] = {
-					config = {
-						icon_preset = "varied",
-					},
-				},
-				["core.mode"] = {},
-				["core.promo"] = {},
-				["core.ui"] = {},
-				["core.itero"] = {},
-				["core.ui.calendar"] = {},
-				["core.queries.native"] = {},
-				["core.summary"] = {},
-				["core.dirman"] = {
-					config = {
-						workspaces = {
-							work = "~/development/notes/work",
-							tn2s = "~/development/notes/TN2S",
-							personal = "~/development/notes/personal",
-						},
-						default_workspace = "personal",
-					},
-				},
-				["core.keybinds"] = {
-					config = {
-						default_keybinds = true,
-						neorg_leader = " ",
-					},
-				},
-			},
-		})
-	end,
-}
+    {
+        "vhyrro/luarocks.nvim",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "nvim-neotest/nvim-nio",
+            "nvim-neorg/lua-utils.nvim",
+            "nvim-lua/plenary.nvim"
+        },		priority = 1000, -- We'd like this plugin to load first out of the rest
+        config = true, -- This automatically runs `require("luarocks-nvim").setup()`
+    },
+    {
+        "nvim-neorg/neorg",
+        priority = 30,
+        dependencies = {
+            { "vhyrro/luarocks.nvim" },
+            { "godlygeek/tabular" },
+        },
+        -- config = true,
+        lazy = true,
+        ft = "norg",
+        cmd = "Neorg",
+        config = function()
+            require("neorg").setup({
+                load = {
+                    ["core.defaults"] = {},
+                    -- ["core.completion"] = {
+                        -- 	config = {
+                            -- 		engine = "nvim-cmp",
+                            -- 	},
+                            -- },
+                            ["core.clipboard.code-blocks"] = {},
+                            ["core.autocommands"] = {},
+                            ["core.esupports.indent"] = {},
+                            ["core.qol.toc"] = {
+                                config = {
+                                    close_split_on_jump = false,
+                                },
+                            },
+                            ["core.integrations.treesitter"] = {},
+                            ["core.export"] = {},
+                            ["core.export.markdown"] = {
+                                config = {
+                                    extension = "md",
+                                    extenstions = "all",
+                                },
+                            },
+                            ["core.concealer"] = {
+                                config = {
+                                    icon_preset = "varied",
+                                },
+                            },
+                            ["core.mode"] = {},
+                            ["core.promo"] = {},
+                            ["core.ui"] = {},
+                            ["core.itero"] = {},
+                            ["core.ui.calendar"] = {},
+                            ["core.queries.native"] = {},
+                            ["core.summary"] = {},
+                            ["core.dirman"] = {
+                                config = {
+                                    workspaces = {
+                                        work = "~/development/notes/work",
+                                        tn2s = "~/development/notes/TN2S",
+                                        personal = "~/development/notes/personal",
+                                    },
+                                    default_workspace = "personal",
+                                },
+                            },
+                            ["core.keybinds"] = {
+                                config = {
+                                    default_keybinds = true,
+                                    neorg_leader = " ",
+                                },
+                            },
+                        },
+                    })
+                end,
+            },
+        }
