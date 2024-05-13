20a16d7cf7f9baf5e13ac142070a1bdf2f01a148
Jeevan Pranav <jellybean02092002@gmail.com>
2024-05-14 01:04:07 +0530

    fix(nvim): made comment plugin lazy load

--- nvim/.config/nvim/lua/plugins/extra/comment.lua
+++ nvim/.config/nvim/lua/plugins/extra/comment.lua
@@ -1,6 +1,7 @@
 return {
     {
         "numToStr/Comment.nvim",
+		lazy = true,
         event = { "BufReadPre", "BufNewFile" },
         config = function()
             require("Comment").setup()
